d4167cbeeaa74fff9b45112073610a8c79ebd55e
Chavdar Bilyanski <chavdar.bilyanski@gmail.com>
2025-07-09 20:59:14 +0300

    Part 3, tested working

--- src/web/powerbidder/settings.py
+++ src/web/powerbidder/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # We will add Whitenoise here later for static files
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -109,3 +109,14 @@ STATICFILES_DIRS = [
 
 # --- Default primary key field type ---
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# --- Static files ---
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# The directory where collectstatic will gather all static files for deployment.
+STATIC_ROOT = BASE_DIR.parent.parent / 'staticfiles'
+# Use Whitenoise's storage backend for compression and caching.
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
